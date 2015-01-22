268574fe270730fbf03bbca17315aa4cd3a01011
wnxhaja <wnxhaja@gmail.com>
2015-01-23 03:44:01 +0800

    Initialize Pongya

---
+++ android/src/com/ajlk/pongya/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ajlk.pongya.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ajlk.pongya.PongYa;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PongYa(), config);
+	}
+}
