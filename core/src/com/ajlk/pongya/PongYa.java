7279aefc636a3dd1141f6ddd4428fab75d12cc84
wnxhaja <wnxhaja@gmail.com>
2015-01-26 23:29:03 +0800

    Update Game Developer Log
    	Able to run game app in android, desktop, and html5

--- core/src/com/ajlk/pongya/PongYa.java
+++ core/src/com/ajlk/pongya/PongYa.java
@@ -3,25 +3,35 @@ package com.ajlk.pongya;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class PongYa extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	private OrthographicCamera camera;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("images/ajlkTransparent.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		camera.update();
+        
+		Gdx.gl.glClearColor(1, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, -20, -50);
 		batch.end();
 	}
+	
+	@Override
+    public void resize(int width, int height) {
+        float aspectRatio = (float) width / (float) height;
+        camera = new OrthographicCamera(2f * aspectRatio, 2f);
+    }
 }
