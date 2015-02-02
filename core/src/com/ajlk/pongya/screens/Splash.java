6437f4677fb87400515c87a974f6cb9a54b97927
wnxhaja <wnxhaja@gmail.com>
2015-02-03 03:34:47 +0800

    Added the game start 	Able to fade in and out welcome screen Changes to be committed: 	modified:   core/src/com/ajlk/pongya/screens/Splash.java 	new file:   core/src/com/ajlk/pongya/tween/SpriteAccessor.java

--- core/src/com/ajlk/pongya/screens/Splash.java
+++ core/src/com/ajlk/pongya/screens/Splash.java
@@ -1,5 +1,9 @@
 package com.ajlk.pongya.screens;
 
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenManager;
+
+import com.ajlk.pongya.tween.SpriteAccessor;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -11,20 +15,29 @@ public class Splash implements Screen{
 	
 	private SpriteBatch batch;
 	private Sprite splash;
+	private TweenManager tweenManager;
 
 	@Override
 	public void show() {
 		batch = new SpriteBatch();
+		tweenManager = new TweenManager();
+		Tween.registerAccessor(Sprite.class, new SpriteAccessor());
 		
 		Texture splashTexture=  new Texture("images/ajlkTransparent.png");
 		splash = new Sprite(splashTexture);
 		splash.setSize(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		
+		Tween.set(splash, SpriteAccessor.ALPHA).target(0).start(tweenManager);
+		Tween.to(splash, SpriteAccessor.ALPHA, 2).target(1).start(tweenManager);
+		Tween.to(splash, SpriteAccessor.ALPHA, 2).target(0).delay(2).start(tweenManager);
 	}
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(1,0,1,1);
+		Gdx.gl.glClearColor(0,0,0,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		tweenManager.update(delta);
 		
 		batch.begin();
 		splash.draw(batch);
