6f490c587feeec9e63d9d36ef72c571b9716d4e5
wnxhaja <wnxhaja@gmail.com>
2015-02-04 01:58:26 +0800

    Creating menu button
    Changes to be committed:
    	new file:   android/assets/ui/bg.png
    	new file:   android/assets/ui/heading.png
    	modified:   core/src/com/ajlk/pongya/screens/MainMenu.java

--- core/src/com/ajlk/pongya/screens/MainMenu.java
+++ core/src/com/ajlk/pongya/screens/MainMenu.java
@@ -2,13 +2,17 @@ package com.ajlk.pongya.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
 public class MainMenu implements Screen {
 	
@@ -22,16 +26,48 @@ public class MainMenu implements Screen {
 
 	@Override
 	public void show() {
+		stage = new Stage();
+
+		Gdx.input.setInputProcessor(stage);
+		
 		atlas = new TextureAtlas("ui/button.pack");
 		skin = new Skin(atlas);
 		
+		table = new Table(skin);
+		table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
 		white = new BitmapFont(Gdx.files.internal("font/white.fnt"),false);
-		black = new BitmapFont(Gdx.files.internal("font/black.fnt"),false);
+		black = new BitmapFont(Gdx.files.internal("font/black.fnt"),false);		
+		
+		//creating buttons
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		textButtonStyle.up = skin.getDrawable("button.up");
+		textButtonStyle.down = skin.getDrawable("button.down");
+		textButtonStyle.pressedOffsetX = 1;
+		textButtonStyle.pressedOffsetY = -1;
+		textButtonStyle.font = black;
+		
+		buttonExit = new TextButton("Exit", textButtonStyle);
+		buttonExit.pad(20);
+		
+		//creating heading
+		LabelStyle headingStyle = new LabelStyle(white, Color.WHITE);
+		heading = new Label("PongYah!", headingStyle);
+
+		
+		table.add(heading);
+		table.row();
+		table.add(buttonExit);
+		stage.addActor(table);
 	}
 
 	@Override
 	public void render(float delta) {
-
+		Gdx.gl.glClearColor(0,0,0,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		stage.act(delta);		
+		stage.draw();
 	}
 
 	@Override
@@ -56,7 +92,7 @@ public class MainMenu implements Screen {
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
+		stage.dispose();
 
 	}
 
