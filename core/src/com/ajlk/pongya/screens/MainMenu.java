be639106cfcca22d30a162e4196ebfebeccc4a05
wnxhaja <wnxhaja@gmail.com>
2015-02-04 00:44:47 +0800

    add ui for main menu Changes to be committed: 	new file:   android/assets/ui/button.pack 	new file:   android/assets/ui/button.png 	modified:   core/src/com/ajlk/pongya/screens/MainMenu.java

--- core/src/com/ajlk/pongya/screens/MainMenu.java
+++ core/src/com/ajlk/pongya/screens/MainMenu.java
@@ -1,12 +1,32 @@
 package com.ajlk.pongya.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class MainMenu implements Screen {
+	
+	private Stage stage;
+	private TextureAtlas atlas;
+	private Skin skin;
+	private Table table;
+	private TextButton buttonExit, buttonPlay;
+	private BitmapFont white,black;
+	private Label heading;
 
 	@Override
 	public void show() {
-
+		atlas = new TextureAtlas("ui/button.pack");
+		skin = new Skin(atlas);
+		
+		white = new BitmapFont(Gdx.files.internal("font/white.fnt"),false);
+		black = new BitmapFont(Gdx.files.internal("font/black.fnt"),false);
 	}
 
 	@Override
