48d6ff1ad4084685ba3ad4d33893903e9cdb1c81
lkb123 <louiebasay@gmail.com>
2015-02-09 04:51:23 +0800

    Created temporary achievement page

--- core/src/com/ajlk/pongya/screens/AchievementScreen.java
+++ core/src/com/ajlk/pongya/screens/AchievementScreen.java
@@ -5,23 +5,103 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class AchievementScreen implements Screen {
-
+	
+	Texture fbImg ;
+	Texture twitterImg;
+	Stage stage;
+	TextureAtlas atlas;
+	Skin skin;
+	Texture headingTexture;
+	Image headingImage;
+	Table table;
+	BitmapFont white;
+	BitmapFont black;
+	TextButton buttonFb, buttonTwitter;
+	
 	@Override
 	public void show() {
-		Gdx.input.setCatchBackKey(true);
+		stage = new Stage(new FitViewport(1280, 720));
+		Gdx.input.setInputProcessor(stage);
+		fbImg = new Texture("sharing/fb.jpg");
+		twitterImg = new Texture("sharing/twitter.jpg");
+		
+		atlas = new TextureAtlas("ui/button.pack");
+		skin = new Skin(atlas);
+		
+		headingTexture =  new Texture("ui/heading.png");
+		headingImage = new Image(headingTexture);
 
+
+		table = new Table(skin);
+		table.setBounds(0, 0, stage.getViewport().getWorldWidth(), stage.getViewport().getWorldHeight());
+		
+		white = new BitmapFont(Gdx.files.internal("font/white.fnt"),false);
+		black = new BitmapFont(Gdx.files.internal("font/black.fnt"),false);		
+		
+		//creating buttons
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		textButtonStyle.up = skin.getDrawable("button.up");
+		textButtonStyle.down = skin.getDrawable("button.down");
+		textButtonStyle.pressedOffsetX = 1;
+		textButtonStyle.pressedOffsetY = -1;
+		textButtonStyle.font = black;
+
+		buttonFb = new TextButton("Facebook", textButtonStyle);
+		buttonFb.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				Gdx.app.exit();
+			}
+			
+		});
+		buttonFb.pad(20);
+		
+		buttonTwitter = new TextButton("Twitter", textButtonStyle);
+		buttonTwitter.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				Gdx.app.exit();
+			}
+			
+		});
+		buttonTwitter.pad(20);
+		
+		table.add(headingImage).spaceBottom(100).minWidth(700).minHeight(100);
+		table.row();
+		table.add(buttonFb).spaceBottom(20).minWidth(250);
+		table.row();
+		table.add(buttonTwitter).spaceBottom(20).minWidth(250);
+		table.row();
+		
+		stage.addActor(table);
+		Gdx.input.setCatchBackKey(true);
 	}
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0,1,0,1);
+		Gdx.gl.glClearColor(0,0,0,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		if (Gdx.input.isKeyPressed(Input.Keys.BACK)) {
 			((Game)Gdx.app.getApplicationListener()).setScreen(new MainMenu());
 		}
+		
+		stage.act(delta);
+		stage.draw();
 	}
 
 	@Override
@@ -50,8 +130,9 @@ public class AchievementScreen implements Screen {
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-
+		stage.dispose();
+		fbImg.dispose();
+		twitterImg.dispose();
 	}
 
 }
