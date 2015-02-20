d245928c88dce29cc10773c31f74245eb3fcb268
wnxhaja <wnxhaja@gmail.com>
2015-02-21 07:41:51 +0800

    Update EnemyPaddle

--- core/src/com/ajlk/pongya/screens/EnemyPaddle.java
+++ core/src/com/ajlk/pongya/screens/EnemyPaddle.java
@@ -10,20 +10,17 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class EnemyPaddle extends Actor{
-	
 	private Viewport viewport;
-	private Texture texture = new Texture(Gdx.files.internal("ui/paddleLeft.png"));
-	private Sprite enemySprite = new Sprite(texture);
+	private  Texture texture = new Texture(Gdx.files.internal("ui/paddleLeft.png"));
+	private  Sprite enemySprite = new Sprite(texture);
 
-	private Vector2 enemyPos;
-	private float defaultVel = 10;
 
 	
 	public EnemyPaddle(Viewport viewport) {
 		this.viewport = viewport;
 		this.setBounds(enemySprite.getX(), enemySprite.getY(), enemySprite.getWidth(), enemySprite.getHeight());
 		this.setPosition(new Vector2(viewport.getWorldWidth(),viewport.getWorldHeight()));
-		this.enemySprite.setSize(viewport.getWorldWidth()/30, viewport.getWorldHeight()/4);
+		enemySprite.setSize(viewport.getWorldWidth()/30, viewport.getWorldHeight()/4);
 		
 	}
 	
@@ -41,20 +38,35 @@ public class EnemyPaddle extends Actor{
 		return enemySprite.getBoundingRectangle();
 	}
 	
-	public void update(){
-		this.enemyPos = new Vector2(enemySprite.getX(),enemySprite.getY());
-
-		enemySprite.setPosition(enemyPos.x, enemyPos.y + defaultVel);
+	public void update(Vector2 pos){
+		float deltaY = enemySprite.getY()- pos.y;
+		float x = enemySprite.getX();
+		
+		if(enemySprite.getY()<viewport.getWorldHeight()-enemySprite.getHeight()&& deltaY<0)
+			enemySprite.setPosition(x, pos.y - enemySprite.getHeight()/2);
+		
+		if(enemySprite.getY()>viewport.getWorldHeight()-enemySprite.getHeight())
+			enemySprite.setPosition(5, viewport.getWorldHeight()-enemySprite.getHeight());
+		
+		
+		
+		if(enemySprite.getY()>5 && deltaY>0)
+			enemySprite.setPosition(x, pos.y - enemySprite.getHeight()/2);
+		
+		if(enemySprite.getY()<5)
+			enemySprite.setPosition(5, 0);
 	}
 
 	public void setPosition(Vector2 position) {
-		this.enemySprite.setPosition(5, position.y/2 - enemySprite.getHeight()/2);
-		
+		enemySprite.setPosition(5, position.y/2 - enemySprite.getHeight()/2);
 	}
+	
 	
 	public void dispose(){
 		texture.dispose();
 	}
+
+	
 
 
 }
